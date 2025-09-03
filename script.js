cb6e3ec3b78048ac24acb24dc6b11c31eccebc8b
tita-16 <00001110632393sp@al.educacao.sp.gov.br>
2025-09-03 13:43:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Lívia Pellegrini| 2ºE| Data:27/08/2025
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
